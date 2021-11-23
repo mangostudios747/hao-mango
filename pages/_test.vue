ac73aed1d5e7e5e1da7a8ef804f28c2fe030ccc1
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-11-22 22:00:31 -0800

    routes?

---
+++ pages/_test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-if="$store.state.currentTest">
+      <h1  class="font-bold text-black leading-4 text-2xl">{{$store.state.testData.name}}</h1>
+      <problem :problem="$store.state.currentProblem"/>
+    </div>
+    <div v-else>
+      {{ chap }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({$content, params}){
+    const { test } = params;
+    const chap = await $content(test).fetch();
+    return {chap};
+  },
+  mounted() {
+    this.$store.dispatch('switchTest', {testID: this.chap.slug, test: this.chap});
+  },
+  beforeRouteUpdate(to, from, next){
+    this.$store.dispatch('switchTest', {testID: this.chap.slug, test: this.chap});
+    next();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="">
-    <div v-if="$store.state.currentTest">
-         <h1  class="font-bold text-black leading-4 text-2xl">{{$store.state.testData.name}}</h1>
-         <problem :problem="$store.state.currentProblem"/>
-    </div>
-    <div v-else>
-   <h1  class="font-bold text-black leading-4 text-2xl">Home</h1>
-   </div>
+    <h1  class="font-bold text-black leading-4 text-2xl">Home</h1>
   </div>
 </template>
 
