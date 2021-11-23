38a1ab8df5ffc01981a6d12c74e9ecc14b79a2d2
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-11-22 22:19:16 -0800

    regular old href

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,10 +8,10 @@
   </nav>
   <div class="flex body h-full flex-row ">
     <nav :class="sidebar?'w-full -mr-80 pl-2 sm:mr-2 flex-auto block  sm:w-80 ':'hidden  sm:block '" v-if="chaps" class="flex flex-col overflow-x-hidden transition-transform duration-300 ml-3 transform mr-2">
-    <router-link tag="a" :to="'/'+chap.slug" :key="chap.slug" v-for="chap of chaps" class="rounded-2xl block my-1 w-full mr-3 py-2 px-4 text-green-500 " :class="$store.state.currentTest == chap.slug? 'bg-gray-200 shadow-inner  pointer-events-non':'hover:shadow-inner hover:bg-gray-200 bg-white shadow cursor-pointer'">
+    <a  :href="'/hao-mango/'+chap.slug" :key="chap.slug" v-for="chap of chaps" class="rounded-2xl block my-1 w-full mr-3 py-2 px-4 text-green-500 " :class="$store.state.currentTest == chap.slug? 'bg-gray-200 shadow-inner  pointer-events-non':'hover:shadow-inner hover:bg-gray-200 bg-white shadow cursor-pointer'">
       <span class="my-2">{{chap.name}}</span>
       <progress-bar class="mt-2 mb-1" :value="0.6"></progress-bar>
-    </router-link>
+    </a>
 
    </nav>
    <div  :class="sidebar?'translate-x-96 sm:translate-x-0':'translate-x-0'" class="py-12 h-full w-full mx-4  rounded-2xl shadow-inner duration-300 transition-transform bg-gray-200 px-2 transform sm:px-16">
