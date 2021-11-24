2ca0e8ae5c466ffda012fd2ad0bcf6ce2fe89c5a
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-11-24 14:20:29 -0800

    more color

--- components/Question.vue
+++ components/Question.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <p class="text-lg" >Translate {{q.data}}</p>
+      <p class="text-2xl mb-2" >Translate <span class="text-green-500 font-semibold ">&ldquo;{{q.data}}&rdquo; </span>:</p>
 
   </div>
 </template>
