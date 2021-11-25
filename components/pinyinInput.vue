0be1248833e1fd10f658496ea9f223b20b8ac184
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-11-24 23:26:27 -0800

    pinyin hints :smile:

--- components/pinyinInput.vue
+++ components/pinyinInput.vue
@@ -3,21 +3,21 @@
     <div class="flex flex-col">
       <div :key="idx" v-for="(c, idx) of data" class="flex mx-auto flex-col">
         <div class="grid grid-cols-4 mx-0 rounded-md">
-          <div @click="input[idx].tone='1'" :class="input[idx].tone==1?'active':''" class="tone-select-box ">
+          <div @click="input[idx].tone='1'" :class="input[idx].tone==1?'active':(wrong&&data[idx].tone==1?'hint':'')" class="tone-select-box ">
             <tone-one/>
           </div>
-          <div @click="input[idx].tone='2'" :class="input[idx].tone==2?'active':''" class="tone-select-box">
+          <div @click="input[idx].tone='2'" :class="input[idx].tone==2?'active':(wrong&&data[idx].tone==2?'hint':'')" class="tone-select-box">
             <tone-two/>
           </div>
-          <div @click="input[idx].tone='3'" :class="input[idx].tone==3?'active':''" class="tone-select-box">
+          <div @click="input[idx].tone='3'" :class="input[idx].tone==3?'active':(wrong&&data[idx].tone==3?'hint':'')" class="tone-select-box">
             <tone-three/>
           </div>
-          <div @click="input[idx].tone='4'" :class="input[idx].tone==4?'active':''" class="tone-select-box">
+          <div @click="input[idx].tone='4'" :class="input[idx].tone==4?'active':(wrong&&data[idx].tone==4?'hint':'')" class="tone-select-box">
             <tone-four/>
           </div>
         </div>
         <div class="rounded-md shadow-inner  bg-gray-200 mt-3 mb-2 focus-within:bg-white focus-within:shadow-md w-full mx-auto">
-          <input  v-model="input[idx]['letters']" class="bg-transparent  focus:outline-none px-3 text-lg w-full" />
+          <input :placeholder="wrong?data[idx].letters:''" v-model="input[idx]['letters']" class="bg-transparent  focus:outline-none px-3 text-lg w-full" />
         </div>
       </div>
     </div>
@@ -102,5 +102,9 @@ export default {
 
 .tone-select-box:last-of-type {
   @apply rounded-r-lg
+}
+
+.tone-select-box.hint {
+  @apply border-4 border-green-500;
 }
 </style>
