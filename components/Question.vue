a1f7ff76692ee945c80ab2655548cdca68dac64c
Saumya Singhal <65318685+saumyasinghal747@users.noreply.github.com>
2021-12-12 18:27:01 -0800

    darking translate

--- components/Question.vue
+++ components/Question.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <p class="text-2xl mb-2" >Translate <span class="text-green-500 font-semibold ">&ldquo;{{q.data}}&rdquo; </span>:</p>
+      <p class="text-2xl dark:text-gray-50 mb-2" >Translate <span class="text-green-500 font-semibold ">&ldquo;{{q.data}}&rdquo; </span>:</p>
 
   </div>
 </template>
