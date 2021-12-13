eb2043b3d4e4b11c372ce9f822342cc7cdd5433e
Saumya Singhal <65318685+saumyasinghal747@users.noreply.github.com>
2021-12-12 18:38:23 -0800

    dark tone one

--- components/tones/tone-one.vue
+++ components/tones/tone-one.vue
@@ -1,6 +1,6 @@
 <template>
   <svg viewBox="0 0 100 100" width="100%" height="100%" preserveAspectRatio="xMidYMin" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" >
-    <line class="stroke-2 stroke-current text-gray-800" x1="0" y1="50" x2="100" y2="50" stroke="black" />
+    <line class="stroke-2 stroke-current text-gray-800" x1="0" y1="50" x2="100" y2="50" class="stroke-current text-black dark:text-gray-50" />
 
     <!-- If you do not specify the stroke
          color the line will not be visible -->
