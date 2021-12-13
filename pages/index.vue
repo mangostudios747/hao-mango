6a3804476da689d90b3367369e3f5881ae70de28
Saumya Singhal <65318685+saumyasinghal747@users.noreply.github.com>
2021-12-12 18:08:32 -0800

    darking mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-    <h1  class="font-bold text-black leading-4 text-2xl">Home</h1>
+    <h1  class="font-bold text-black dark:text-gray-50 leading-4 text-2xl">Home</h1>
   </div>
 </template>
 
