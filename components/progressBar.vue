175ab42c613a810302a5e73f37a3b7d369b42684
Saumya Singhal <65318685+saumyasinghal747@users.noreply.github.com>
2021-12-12 18:35:25 -0800

    fine i won’t

--- components/progressBar.vue
+++ components/progressBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-3 relative  max-w-xl rounded-full overflow-hidden">
-    <div class="w-full shadow-inner h-full bg-gray-300 dark:bg-gray-900 absolute"></div>
+    <div class="w-full shadow-inner h-full bg-gray-300 dark:bg-gray-700 absolute"></div>
     <div :class="`bg-${color}`" class="h-full transition-all duration-200 shadow-2xl rounded-full absolute" :style="`width:${value*100}%`"></div>
   </div>
 </template>
