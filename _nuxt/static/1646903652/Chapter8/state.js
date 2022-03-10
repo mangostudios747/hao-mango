fad97b8101c9f7f45086dd85deed85e2a886c8ef
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2022-03-10 22:22:54 +0000

    deploy: cd842eea50b991b6422a329303c585f02521bddd

--- _nuxt/static/1646903652/Chapter8/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646903652",layout:"default",error:b,state:{currentTest:a,testData:a,currentProblem:a,testProgress:a,skipToHandwriting:a},serverRendered:true,routePath:"\u002FChapter8",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:b},content:{dbHash:"6f5738f1"}}}}(false,null));
---
+++ _nuxt/static/1646950973/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646950973",layout:"default",error:b,state:{currentTest:a,testData:a,currentProblem:a,testProgress:a,skipToHandwriting:a},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:b},content:{dbHash:"2c55372d"}}}}(false,null,"\u002F"));
