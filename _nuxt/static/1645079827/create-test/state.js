98a664bbd114d719ff487571ad24b28601daed42
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2022-03-10 09:06:29 +0000

    deploy: 12cc54aae2bad9d8ba896fe075c2b7cc8f338ce8

--- _nuxt/static/1645079827/create-test/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645079827",layout:"default",error:b,state:{currentTest:a,testData:a,currentProblem:a,testProgress:a},serverRendered:true,routePath:"\u002Fcreate-test",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:b},content:{dbHash:"ff421243"}}}}(false,null));
--- _nuxt/static/1645079827/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645079827",layout:"default",error:b,state:{currentTest:a,testData:a,currentProblem:a,testProgress:a},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:b},content:{dbHash:"ff421243"}}}}(false,null,"\u002F"));
